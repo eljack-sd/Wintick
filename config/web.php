64ed3387735d6869d880c627aff68c6c6c2beab7
Ociel <consultoria@winetpc.com>
2025-11-19 13:57:41 -0600

    Espacios enter

--- config/web.php
+++ config/web.php
@@ -14,6 +14,11 @@ $config = [
     'language' => "es-ES",
     'components' => [
         'request' => [
+
+
+
+
+
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'fk3TG_tHdrVbcTFW13tFkkK5RmwTCPbe',
             'enableCsrfValidation' => false,
